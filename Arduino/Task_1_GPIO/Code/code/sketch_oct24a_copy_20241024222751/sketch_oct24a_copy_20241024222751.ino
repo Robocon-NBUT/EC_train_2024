9e74b99f5bd1fd59460aad4291b797499c6fc7d6
zzyb06 <1479336168@qq.com>
2024-10-26 15:27:55 +0800

    robot242zzyb

--- Arduino/Task_1_GPIO/Code/code/sketch_oct24a_copy_20241024222751/sketch_oct24a_copy_20241024222751.ino
+++ Arduino/Task_1_GPIO/Code/code/sketch_oct24a_copy_20241024222751/sketch_oct24a_copy_20241024222751.ino
@@ -1,7 +1,7 @@
 void setup() {
   pinMode(13,OUTPUT);//设置引脚13为输出
   pinMode(9,OUTPUT);//设置引脚9为输出
-  pinMode(7,INPUT);//设置引脚7为输入
+  pinMode(3,INPUT);//设置引脚3为输入
 }
 
 void loop() {
@@ -10,13 +10,19 @@ delay(500);
 digitalWrite(13,LOW);
 delay(500);//实现板上Led以1hz切换
 
-  if(digitalRead(7) == HIGH){
-    digitalWrite(9,HIGH);
-    delay(500);
-    digitalWrite(9,LOW);
-    delay(500);
-  }
+if(digitalRead(3)==HIGH){//实现两个引脚状态相同
+for(int i=0;i<=255;i++)
+{
+  analogWrite(9,i);
+  delay(5);
+}
+for(int i=255;i>=0;i--)
+{
+analogWrite(9,i);
+delay(5);
+}
+}
 else{
   digitalWrite(9,LOW);
-}//实现不同引脚的状态相同
+  }//实现呼吸灯
 }
