176b597896a630358285ba8a1dca0b1d6a5fc770
tomorrow39 <3490209041@qq.com>
2024-10-26 11:53:06 +0800

    Update input_output.ino

--- Arduino/Task_1_GPIO/Code/input_output.ino
+++ Arduino/Task_1_GPIO/Code/input_output.ino
@@ -1,9 +1,11 @@
 void setup() {
   pinMode(3, INPUT); 
   pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(4, HIGH);
   int open = digitalRead(3); 
   digitalWrite(5, open);
 }
