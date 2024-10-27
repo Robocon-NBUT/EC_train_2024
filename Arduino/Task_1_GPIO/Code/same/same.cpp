88e25b419cc466b960163f928c4473354287f9ec
Your Name <you@example.com>
2024-10-27 17:58:29 +0800

    4

--- Arduino/Task_1_GPIO/Code/same/same.cpp
+++ Arduino/Task_1_GPIO/Code/same/same.cpp
@@ -2,18 +2,18 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(3,OUTPUT);
   pinMode(9,OUTPUT);
-  pinMode(11,OUTPUT);
+  pinMode(13,OUTPUT);
 
 }
 
 void loop() {
   digitalWrite(3,HIGH);
   digitalWrite(9,HIGH);
-  digitalWrite(11,HIGH);
+  digitalWrite(13,HIGH);
   delay(1000);
   digitalWrite(3,LOW);
   digitalWrite(9,LOW);
-  digitalWrite(11,LOW);
+  digitalWrite(13,LOW);
   delay(1000);
   // put your main code here, to run repeatedly:
 
