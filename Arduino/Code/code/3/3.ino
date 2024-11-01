f18055d321fdfb9e123d0b181a40967a76afd69c
zzyb06 <1479336168@qq.com>
2024-11-01 17:47:43 +0800

    zhuzhangyibo

--- Arduino/Code/code/3/3.ino
+++ Arduino/Code/code/3/3.ino
@@ -8,13 +8,6 @@ for(int i=0;i<=255;i++)//循环使i模拟量升高使灯变亮
   analogWrite(9,i);
   delay(5);
 }
-for(int x=0;x<=10;x++){
-digitalWrite(9,HIGH);
-delay(50);
-digitalWrite(9,LOW);
-delay(50);
-}
-
 for(int i=255;i>=0;i--)//循环使i模拟量降低使灯变暗
 {
 analogWrite(9,i);
