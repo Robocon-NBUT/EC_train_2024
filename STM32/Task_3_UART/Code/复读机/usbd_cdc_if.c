27292998b155f120bbf7e3b318401fdb50b556a1
zzyb06 <1479336168@qq.com>
2024-11-11 22:34:49 +0800

    Update usbd_cdc_if.c

--- STM32/Task_3_UART/Code/复读机/usbd_cdc_if.c
+++ STM32/Task_3_UART/Code/复读机/usbd_cdc_if.c
@@ -258,8 +258,7 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   * @param  Len: Number of data received (in bytes)
   * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
   */
-static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
-{
+
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
     char myStr[64] = {0};
