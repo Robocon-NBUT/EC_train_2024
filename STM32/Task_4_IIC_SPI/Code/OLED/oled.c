a421d36c6d49d2f903013e2376eddf8a655cd26a
chenzhe-sdhjajk <2093613909@qq.com>
2024-11-21 15:47:07 +0800

    789

---
+++ STM32/Task_4_IIC_SPI/Code/OLED/oled.c
@@ -0,0 +1,158 @@
+#include "oled.h"
+#include "stdlib.h"
+#include "oledfont.h"
+#include "main.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+#define OLED_ADD 0x78
+
+void Write_IIC_Command(unsigned char i2c_cmd)
+{
+    uint8_t *cmd;
+    cmd = &i2c_cmd;
+    HAL_I2C_Mem_Write(&hi2c1, OLED_ADD, 0x00, I2C_MEMADD_SIZE_8BIT, cmd, 1, 100);
+}
+
+void Write_IIC_Data(unsigned char i2c_data)
+{
+    uint8_t *data;
+    data = &i2c_data;
+    HAL_I2C_Mem_Write(&hi2c1, OLED_ADD, 0x40, I2C_MEMADD_SIZE_8BIT, data, 1, 100);
+}
+
+void OLED_WR_Byte(unsigned char dat, unsigned char cmd)
+{
+    if (cmd)
+    {
+        Write_IIC_Data(dat);
+    }
+    else
+    {
+        Write_IIC_Command(dat);
+    }
+}
+
+// 坐标设置
+
+void OLED_Set_Pos(unsigned char x, unsigned char y)
+{
+    OLED_WR_Byte(0xb0 + y, OLED_CMD);
+    OLED_WR_Byte(((x & 0xf0) >> 4) | 0x10, OLED_CMD);
+    OLED_WR_Byte((x & 0x0f), OLED_CMD);
+}
+// 开启OLED显示
+void OLED_Display_On(void)
+{
+    OLED_WR_Byte(0X8D, OLED_CMD); // SET DCDC命令
+    OLED_WR_Byte(0X14, OLED_CMD); // DCDC ON
+    OLED_WR_Byte(0XAF, OLED_CMD); // DISPLAY ON
+}
+// 关闭OLED显示
+void OLED_Display_Off(void)
+{
+    OLED_WR_Byte(0X8D, OLED_CMD); // SET DCDC命令
+    OLED_WR_Byte(0X10, OLED_CMD); // DCDC OFF
+    OLED_WR_Byte(0XAE, OLED_CMD); // DISPLAY OFF
+}
+// 清屏函数,清完屏,整个屏幕是黑色的!和没点亮一样!!!
+void OLED_Clear(void)
+{
+    uint8_t i, n;
+    for (i = 0; i < 8; i++)
+    {
+        OLED_WR_Byte(0xb0 + i, OLED_CMD); // 设置页地址（0~7）
+        OLED_WR_Byte(0x00, OLED_CMD);     // 设置显示位置—列低地址
+        OLED_WR_Byte(0x10, OLED_CMD);     // 设置显示位置—列高地址
+        for (n = 0; n < 128; n++)
+            OLED_WR_Byte(0, OLED_DATA);
+    } // 更新显示
+}
+void OLED_On(void)
+{
+    uint8_t i, n;
+    for (i = 0; i < 8; i++)
+    {
+        OLED_WR_Byte(0xb0 + i, OLED_CMD); // 设置页地址（0~7）
+        OLED_WR_Byte(0x00, OLED_CMD);     // 设置显示位置—列低地址
+        OLED_WR_Byte(0x10, OLED_CMD);     // 设置显示位置—列高地址
+        for (n = 0; n < 128; n++)
+            OLED_WR_Byte(1, OLED_DATA);
+    } // 更新显示
+}
+
+// 显示汉字
+void OLED_ShowCHinese(uint8_t x, uint8_t y, uint8_t no)
+{
+    uint8_t t, adder = 0;
+    OLED_Set_Pos(x, y);
+    for (t = 0; t < 16; t++)
+    {
+        OLED_WR_Byte(Hzk[2 * no][t], OLED_DATA);
+        adder += 1;
+    }
+    OLED_Set_Pos(x, y + 1);
+    for (t = 0; t < 16; t++)
+    {
+        OLED_WR_Byte(Hzk[2 * no + 1][t], OLED_DATA);
+        adder += 1;
+    }
+}
+/***********功能描述：显示显示BMP图片128×64起始点坐标(x,y),x的范围0～127，y为页的范围0～7*****************/
+void OLED_DrawBMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[])
+{
+    unsigned int j = 0;
+    unsigned char x, y;
+
+    if (y1 % 8 == 0)
+        y = y1 / 8;
+    else
+        y = y1 / 8 + 1;
+    for (y = y0; y < y1; y++)
+    {
+        OLED_Set_Pos(x0, y);
+        for (x = x0; x < x1; x++)
+        {
+            OLED_WR_Byte(BMP[j++], OLED_DATA);
+        }
+    }
+}
+
+// 初始化SSD1306
+void OLED_Init(void)
+{
+    OLED_WR_Byte(0xAE, OLED_CMD); //--display off
+    OLED_WR_Byte(0x00, OLED_CMD); //---set low column address
+    OLED_WR_Byte(0x10, OLED_CMD); //---set high column address
+    OLED_WR_Byte(0x40, OLED_CMD); //--set start line address
+    OLED_WR_Byte(0xB0, OLED_CMD); //--set page address
+    OLED_WR_Byte(0x81, OLED_CMD); // contract control
+    OLED_WR_Byte(0xFF, OLED_CMD); //--128
+    OLED_WR_Byte(0xA1, OLED_CMD); // set segment remap
+    OLED_WR_Byte(0xA6, OLED_CMD); //--normal / reverse
+    OLED_WR_Byte(0xA8, OLED_CMD); //--set multiplex ratio(1 to 64)
+    OLED_WR_Byte(0x3F, OLED_CMD); //--1/32 duty
+    OLED_WR_Byte(0xC8, OLED_CMD); // Com scan direction
+    OLED_WR_Byte(0xD3, OLED_CMD); //-set display offset
+    OLED_WR_Byte(0x00, OLED_CMD); //
+
+    OLED_WR_Byte(0xD5, OLED_CMD); // set osc division
+    OLED_WR_Byte(0x80, OLED_CMD); //
+
+    OLED_WR_Byte(0xD8, OLED_CMD); // set area color mode off
+    OLED_WR_Byte(0x05, OLED_CMD); //
+
+    OLED_WR_Byte(0xD9, OLED_CMD); // Set Pre-Charge Period
+    OLED_WR_Byte(0xF1, OLED_CMD); //
+
+    OLED_WR_Byte(0xDA, OLED_CMD); // set com pin configuartion
+    OLED_WR_Byte(0x12, OLED_CMD); //
+
+    OLED_WR_Byte(0xDB, OLED_CMD); // set Vcomh
+    OLED_WR_Byte(0x30, OLED_CMD); //
+
+    OLED_WR_Byte(0x8D, OLED_CMD); // set charge pump enable
+    OLED_WR_Byte(0x14, OLED_CMD); //
+
+    OLED_WR_Byte(0xAF, OLED_CMD); //--turn on oled panel
+}
---
+++ STM32/Task_4_IIC_SPI/Code/ST7789LCD/ST7789.h
@@ -0,0 +1,97 @@
+#define __ST7789_H
+
+#include "fonts.h"
+#include "main.h"
+
+/* choose a Hardware SPI port to use. */
+#define ST7789_SPI_PORT hspi1
+extern SPI_HandleTypeDef ST7789_SPI_PORT;
+
+#define ST7789_RST_PORT GPIOC
+#define ST7789_RST_PIN  GPIO_PIN_4
+#define ST7789_DC_PORT  GPIOA
+#define ST7789_DC_PIN   GPIO_PIN_6
+
+#define ST7789_CS_PORT GPIOA
+#define ST7789_CS_PIN GPIO_PIN_4
+
+#define ST7789_ROTATION 0
+#if ST7789_ROTATION == 0
+#define ST7789_WIDTH 135
+#define ST7789_HEIGHT 240
+#define X_SHIFT 53
+#define Y_SHIFT 40
+#endif
+
+#define WHITE       0xFFFF
+#define BLACK       0x0000
+#define BLUE        0x001F
+#define RED         0xF800
+#define MAGENTA     0xF81F
+#define GREEN       0x07E0
+#define CYAN        0x7FFF
+#define YELLOW      0xFFE0
+#define GRAY        0X8430
+#define BRED        0XF81F
+#define GRED        0XFFE0
+#define GBLUE       0X07FF
+#define BROWN       0XBC40
+#define BRRED 0XFC07
+#define DARKBLUE 0X01CF
+#define LIGHTBLUE 0X7D7C
+#define GRAYBLUE 0X5458
+
+#define LIGHTGREEN 0X841F
+#define LGRAY 0XC618
+#define LGRAYBLUE 0XA651
+#define LBBLUE 0X2B12
+
+#define ST7789_NOP 0x00
+#define ST7789_SWRESET 0x01
+#define ST7789_RDDID 0x04
+#define ST7789_RDDST 0x09
+
+#define ST7789_SLPIN 0x10
+#define ST7789_SLPOUT 0x11
+#define ST7789_PTLON 0x12
+#define ST7789_NORON 0x13
+
+#define ST7789_INVOFF 0x20
+#define ST7789_INVON 0x21
+#define ST7789_DISPOFF 0x28
+#define ST7789_DISPON 0x29
+#define ST7789_CASET 0x2A
+#define ST7789_RASET 0x2B
+#define ST7789_RAMWR 0x2C
+#define ST7789_RAMRD 0x2E
+
+#define ST7789_PTLAR 0x30
+#define ST7789_COLMOD 0x3A
+#define ST7789_MADCTL 0x36
+
+#define ST7789_MADCTL_MY 0x80
+#define ST7789_MADCTL_MX 0x40
+#define ST7789_MADCTL_MV 0x20
+#define ST7789_MADCTL_ML 0x10
+#define ST7789_MADCTL_RGB 0x00
+
+#define ST7789_RDID1 0xDA
+#define ST7789_RDID2 0xDB
+#define ST7789_RDID3 0xDC
+#define ST7789_RDID4 0xDD
+#define ST7789_COLOR_MODE_16bit 0x55 //  RGB565 (16bit)
+#define ST7789_COLOR_MODE_18bit 0x66 //  RGB666 (18bit)
+
+#define ST7789_RST_Clr() HAL_GPIO_WritePin(ST7789_RST_PORT, ST7789_RST_PIN, GPIO_PIN_RESET)
+#define ST7789_RST_Set() HAL_GPIO_WritePin(ST7789_RST_PORT, ST7789_RST_PIN, GPIO_PIN_SET)
+
+#define ST7789_DC_Clr() HAL_GPIO_WritePin(ST7789_DC_PORT, ST7789_DC_PIN, GPIO_PIN_RESET)
+#define ST7789_DC_Set() HAL_GPIO_WritePin(ST7789_DC_PORT, ST7789_DC_PIN, GPIO_PIN_SET)
+#define ST7789_Select() HAL_GPIO_WritePin(ST7789_CS_PORT, ST7789_CS_PIN, GPIO_PIN_RESET)
+#define ST7789_UnSelect() HAL_GPIO_WritePin(ST7789_CS_PORT, ST7789_CS_PIN, GPIO_PIN_SET)
+
+void ST7789_Init(void);
+void ST7789_SetRotation(uint8_t m);
+void ST7789_Fill_Color(uint16_t color);
+void ST7789_Test(void);
+void LCD_ShowPicture(uint16_t x,uint16_t y,uint16_t length,uint16_t width,const uint16_t pic[]);
