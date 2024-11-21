04e48da14fd4fe070bd71cb83df595ee98c60504
orientcg <wxzwxzwxz404@163.com>
2024-11-21 19:08:04 +0800

    wxz

---
+++ STM32/Task_4_IIC_SPI/spi/fonts.h
@@ -0,0 +1,14 @@
+#ifndef __FONT_H
+#define __FONT_H
+
+#include "stdint.h"
+
+typedef struct {
+    const uint8_t width;
+    uint8_t height;
+    const uint16_t *data;
+} FontDef;
+
+extern const uint16_t wxz[][36458];
+
+#endif
